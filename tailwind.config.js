324568f40a049e0419c1591ab5e78fc62ab82a4f
Jerome Manzano <jeromemanzano@gmail.com>
2022-11-15 10:39:04 +0800

    Add apollo, tailwind, hero icons and graphql

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors:{
+      transparent: 'transparent',
+      currentColor: 'currentColor',
+      'black': '#181818',
+      'white': '#FFFFFF',
+      'aureolin': '#FCEE0A',
+      'aqua': '#00F0FF',
+      'candyAppleRed': '#FF0F00',
+      'steelPink': '#CB1DCD',
+      'frostBite': '#E455AE',
+      'blushingPurple': '#9370DB'
+    },
+
     extend: {},
   },
   plugins: [],
